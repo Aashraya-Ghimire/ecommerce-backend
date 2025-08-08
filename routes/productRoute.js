1f696d898304a18fafb03ab6697182afaf50f40c
Aashraya <126077418+Aashraya-Ghimire@users.noreply.github.com>
2025-08-08 15:02:53 +0545

    ecom

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const Product = require("../model/productModel");
+const { jwtAuthMiddleWare } = require("../jwt");
 
 route.get("/", async (req, res) => {
   try {
@@ -13,7 +14,7 @@ route.get("/", async (req, res) => {
   }
 });
 
-route.post("/", async (req, res) => {
+route.post("/", jwtAuthMiddleWare, async (req, res) => {
   try {
     const data = req.body;
     const product = new Product(data);
